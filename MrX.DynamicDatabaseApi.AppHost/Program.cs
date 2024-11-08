b4a4c07aa73067e0cf9c4e3f229a5c6eff0fcd38
Ali Hagh <alihagh1383@gmail.com>
2024-11-08 22:54:32 +0330

    .

--- MrX.DynamicDatabaseApi.AppHost/Program.cs
+++ MrX.DynamicDatabaseApi.AppHost/Program.cs
@@ -1,11 +1,11 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var SQL = builder.AddMySql("SQL");
-var DB = SQL.AddDatabase("DB");
-var SEQ = builder.AddSeq("SEQ");
+var SQL = builder.AddMySql("SQL",port:8000);
+var DB = SQL.AddDatabase("DB","DB");
+var SEQ = builder.AddSeq("SEQ",8080);
 var DDA = builder.AddProject<Projects.MrX_DynamicDatabaseApi_Api>("mrx-dynamicdatabaseapi-api", c => c.ExcludeLaunchProfile = true);
-SQL.WithDataVolume("SQL");
-SEQ.WithDataVolume("SEQ");
+//SQL.WithDataVolume("SQL");
+//SEQ.WithDataVolume("SEQ");
 DDA.WaitFor(DB);
 DDA.WaitFor(SEQ);
 DDA.WithReference(DB);
