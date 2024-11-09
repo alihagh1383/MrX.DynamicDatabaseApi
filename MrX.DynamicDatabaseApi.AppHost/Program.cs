f29183287b5cfbe6df59feb937bc34b542ff8acb
Ali Hagh <alihagh1383@gmail.com>
2024-11-09 09:38:09 +0330

    SQL Create DB error

--- MrX.DynamicDatabaseApi.AppHost/Program.cs
+++ MrX.DynamicDatabaseApi.AppHost/Program.cs
@@ -1,15 +1,16 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var SQL = builder.AddMySql("SQL",port:8000);
-var DB = SQL.AddDatabase("DB","DB");
-var SEQ = builder.AddSeq("SEQ",8080);
+var SQL = builder.AddMySql("SQL",port:8000).PublishAsContainer();
+var DB = SQL.AddDatabase("DB");
+var SEQ = builder.AddSeq("SEQ",8080).PublishAsContainer();
 var DDA = builder.AddProject<Projects.MrX_DynamicDatabaseApi_Api>("mrx-dynamicdatabaseapi-api", c => c.ExcludeLaunchProfile = true);
-//SQL.WithDataVolume("SQL");
-//SEQ.WithDataVolume("SEQ");
+SQL.WithDataVolume("SQL");
+SEQ.WithDataVolume("SEQ");
 DDA.WaitFor(DB);
 DDA.WaitFor(SEQ);
 DDA.WithReference(DB);
 DDA.WithReference(SEQ);
 DDA.WithHttpEndpoint(80);
 DDA.WithExternalHttpEndpoints();
+DDA.AsHttp2Service();
 builder.Build().Run();
