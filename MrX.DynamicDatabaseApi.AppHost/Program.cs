d6dd2f0b313a5393741cd0a278776153a09913f9
Ali Hagh <alihagh1383@gmail.com>
2024-11-09 10:21:12 +0330

    port 80 non local

--- MrX.DynamicDatabaseApi.AppHost/Program.cs
+++ MrX.DynamicDatabaseApi.AppHost/Program.cs
@@ -1,8 +1,8 @@
-var builder = DistributedApplication.CreateBuilder(args);
+var builder = DistributedApplication.CreateBuilder(new DistributedApplicationOptions() { AllowUnsecuredTransport = true });
 
-var SQL = builder.AddMySql("SQL",port:8000).PublishAsContainer();
+var SQL = builder.AddMySql("SQL", port: 8000).PublishAsContainer();
 var DB = SQL.AddDatabase("DB");
-var SEQ = builder.AddSeq("SEQ",8080).PublishAsContainer();
+var SEQ = builder.AddSeq("SEQ", 8080).PublishAsContainer();
 var DDA = builder.AddProject<Projects.MrX_DynamicDatabaseApi_Api>("mrx-dynamicdatabaseapi-api", c => c.ExcludeLaunchProfile = true);
 SQL.WithDataVolume("SQL");
 SEQ.WithDataVolume("SEQ");
@@ -10,7 +10,8 @@ DDA.WaitFor(DB);
 DDA.WaitFor(SEQ);
 DDA.WithReference(DB);
 DDA.WithReference(SEQ);
-DDA.WithHttpEndpoint(80);
+//DDA.WithHttpEndpoint(80);
 DDA.WithExternalHttpEndpoints();
+DDA.WithArgs("--urls", "http://[::]:80");
 DDA.AsHttp2Service();
 builder.Build().Run();
