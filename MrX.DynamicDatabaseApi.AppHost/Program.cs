634ad7ede1ab1cb21bbd401454cf6dcde34bab2f
Ali Hagh <alihagh1383@gmail.com>
2024-11-14 15:44:44 +0330

    Revert "open port by args"
    This reverts commit aee217a1d1614e2c14ca895191ddf1daedfaaca1.

--- MrX.DynamicDatabaseApi.AppHost/Program.cs
+++ MrX.DynamicDatabaseApi.AppHost/Program.cs
@@ -1,4 +1,5 @@
 var builder = DistributedApplication.CreateBuilder(new DistributedApplicationOptions() { AllowUnsecuredTransport = true });
+
 var SQL = builder.AddMySql("SQL", port: 8000).PublishAsContainer();
 var DB = SQL.AddDatabase("DB");
 var SEQ = builder.AddSeq("SEQ", 8080).PublishAsContainer();
@@ -9,27 +10,8 @@ DDA.WaitFor(DB);
 DDA.WaitFor(SEQ);
 DDA.WithReference(DB);
 DDA.WithReference(SEQ);
+//DDA.WithHttpEndpoint(80);
 DDA.WithExternalHttpEndpoints();
+DDA.WithArgs("--urls", "http://[::]:80");
 DDA.AsHttp2Service();
-
-
-var urls = "";
-for (int i = 0; i < args.Length; i++)
-{
-    if (args[i].ToLower() == "--port" && args.Length > i)
-    {
-        foreach (var arg in args[i + 1].Split(','))
-            if (int.TryParse(arg, out int Port) && Port > 0 && Port < 65000)
-            {
-                urls += $";http://*:{Port}";
-                DDA.WithEndpoint(port: Port, targetPort: Port, scheme: "http", name: $"porthttp{Port}", isProxied: false, isExternal: true);
-            }
-    }
-}
-if (!string.IsNullOrWhiteSpace(urls))
-    DDA.WithArgs("--urls", urls.Trim(';'));
-var app = builder.Build();
-app.Run();
-
-
-
+builder.Build().Run();
